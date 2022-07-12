24afa410aafa503c9569d8ccbbf3deb915ac8c2a
Sarthak-22 <sarthakvora22@gmail.com>
2022-07-12 17:51:51 +0530

    Modified run script

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
-CUDA_VISIBLE_DEVICES=2,3 python src/run.py +experiment=[blobgan,local,jitter] wandb.name='Exp_2' \
+CUDA_VISIBLE_DEVICES=1 python src/run.py +experiment=[blobgan,local,jitter] wandb.name='Exp_2_10'    \
 dataset.dataloader.batch_size=32 \
 +model.log_fid_every_epoch=False \
 dataset=multi_clevr \
 +dataset.cameras=[Camera,Camera.001] \
-+dataset.path=datasets/CLEVR_v1.0 model.n_features=3
++dataset.path=datasets/CLEVR_v1.0    \
+model.n_features=10                  \
